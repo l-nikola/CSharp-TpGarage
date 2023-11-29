c3200526544647b032cc1e5d76fae30067d663bf
Nikola Legay <147250203+l-nikola@users.noreply.github.com>
2023-11-29 18:17:09 +0100

    CSharp-TpGarage

---
+++ TPgarrage/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TPgarrage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numero, modele, immat;
+            int annee, marque;
+            double prix;
+            bool plaque;
+            plaque = false;
+
+            Console.WriteLine("Entrez l'immatriculation de la voiture ?");
+            numero = Console.ReadLine();
+            Console.WriteLine("Entrez l'année de mise en circulation ?");
+            annee = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez le code de la marque ?");
+            marque = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez le modèle du véhicule ?");
+            modele = Console.ReadLine();
+            Console.WriteLine("Entrez le prix de la voiture ?");
+            prix = Double.Parse(Console.ReadLine());
+
+            List<Voiture> monGarage = new List<Voiture>();
+            Voiture voit1 = new Voiture("122XZERT", 2001, 1, "Saxo", 2345.50);
+            Console.WriteLine("Numéro : " + voit1.getNumero());
+            voit1.afficher();
+            Voiture voit2 = new Voiture("187ASRT", 2010, 2, "C3", 4500.50);
+            voit2.afficher();
+            voit2.setPrix(4000.00);
+            Voiture voit3 = new Voiture(numero, annee, marque, modele, prix);
+            voit3.afficher();
+            monGarage.Add(voit1);
+            monGarage.Add(voit2);
+            Console.WriteLine();
+            
+            foreach (Voiture voit in monGarage)
+            {
+                voit1.afficher();
+                Console.WriteLine("L'age de la voiture est " + voit.calculAge());
+                Console.WriteLine("Le modèle de la voiture est " + voit.getModele());
+                Console.WriteLine("La marque de la voiture est " + voit.getMarque());
+            }
+
+            Console.WriteLine("Tapez l'immatriculation de la voiture pour la supprimer");
+            immat = Console.ReadLine();
+            foreach (Voiture voit in monGarage)
+            {
+                if (voit.getNumero() == immat)
+                {
+                    plaque = true;
+                }
+
+                if (plaque == true)
+                {
+                    monGarage.Remove(voit);
+                }
+            }
+
+            int i;
+            i = 0;
+            
+            for (i = 0; i < monGarage.Count; i++)
+            {
+                monGarage[i].afficher();
+                Console.WriteLine("L'age de la voiture est " + monGarage[i].calculAge());
+                Console.WriteLine("Le modèle de la voiture est " + monGarage[i].getModele());
+                Console.WriteLine("La marque de la voiture est " + monGarage[i].getMarque());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TPgarrage/Voiture.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using Xamarin.Forms;
+
+namespace TPgarrage
+{
+    public class Voiture
+    {
+        private string _numero;
+        private int _annee;
+        private int _marque;
+        private string _modele;
+        private double _prix;
+        //classes
+        public Voiture (string numero, int annee, int marque, string modele, double prix)
+        {
+            _numero = numero;
+            _annee = annee;
+            _marque = marque;
+            _modele = modele;
+            _prix = prix;
+        }
+        //accensseur numéro
+        public string getNumero()
+        {
+            return _numero;
+        }
+        //accensseur année
+        public int getAnnee()
+        {
+            return _annee;
+        }
+        //accensseur marque
+        public int getMarque()
+        {
+            return _marque;
+        }
+        //accensseur modèle
+        public string getModele()
+        {
+            return _modele;
+        }
+        //accensseur prix
+        public double getPrix()
+        {
+            return _prix;
+        }
+        //modification prix
+        public void setPrix(double nouveauprix)
+        {
+            _prix = nouveauprix;
+        }
+        //calcul age 
+        public int calculAge()
+        {
+            return (DateTime.Today.Year - _annee);
+        }
+        //affichage
+        public void afficher()
+        {
+            Console.WriteLine("L'immatriculation est " + _numero);
+            Console.WriteLine("Le modèle est " + _modele);
+            Console.WriteLine("Le prix est " + _prix);
+            Console.WriteLine("----------------------------");
+        }
+    }
+}
